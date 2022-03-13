40f7c5d4e8f6a0314167426180660d14cb0f7b2f
atmeed <leo.zemtsoff@gmail.com>
2022-03-13 12:16:11 +0300

    Initial Commit

---
+++ Practice_4_sem2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practice_4_sem2
+//
+//  Created by Leonid Zemtsov on 13.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
